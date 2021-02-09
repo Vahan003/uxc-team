508553899116f957bf67a02a124c0ee1f8436798
Muradyan Vahan <mvahan.m@gmail.com>
2021-02-09 23:53:46 +0400

    awesome

--- src/components/Preview.js
+++ src/components/Preview.js
@@ -3,7 +3,7 @@ import { ReactComponent as ArrowP } from "./icon/arrow-to-bottom.svg";
 import "../styles/preview.sass";
 const Preview = (props) => {
   return (
-    <div className="preview" style={{backgroundImage: "url('/images/Rectangle-1.png')"}}>
+    <div className="preview" style={{backgroundImage: "url('./images/Rectangle-1.png')"}}>
       <h1 className="title">Create your comfort zone.</h1>
       <button className="scroll-button" onClick={props.click}>Shop now</button>
       <ArrowP className="arrow-svg" />
